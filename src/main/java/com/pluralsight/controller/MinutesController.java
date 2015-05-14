a2d5981a4e3ace6fbb9de0bc250c238a991d853d
CANTO005 <omar.a.cm@hotmail.com>
2015-05-14 16:54:38 -0500

    Changes

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -17,4 +17,12 @@ public class MinutesController {
 		return "addMinutes";
 	}
 	
+/*	@RequestMapping(value="/addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute ("exercise") Exercise exercise) {
+		
+		System.out.println("exercising: " + exercise.getMinutes());
+		
+		return "addMinutes";
+	}*/
+	
 }
